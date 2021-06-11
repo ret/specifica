7afd6ce1c1e91da006a4e5861ca8615debf40fff
Reto Kramer <kramer@acm.org>
2021-06-10 18:07:20 -0700

    Introduce tla_v to parse and evaluate inlined values.

--- tlaplus-qq/src/Language/TLAPlus/Quasiquote.hs
+++ tlaplus-qq/src/Language/TLAPlus/Quasiquote.hs
@@ -3,6 +3,7 @@
 --   https://markkarpov.com/tutorial/th.html#example-2-creating-refined-values-at-compile-time
 
 {-# LANGUAGE DeriveLift         #-}
+{-# LANGUAGE FlexibleInstances  #-}
 {-# LANGUAGE QuasiQuotes        #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell    #-}
@@ -13,11 +14,15 @@ import           Language.Haskell.TH
 import           Language.Haskell.TH.Quote
 import           Language.Haskell.TH.Syntax
 
+import           Language.TLAPlus.Eval
 import           Language.TLAPlus.Parser           (expression, mkState)
 import           Language.TLAPlus.Pretty           (prettyPrintE, prettyPrintVA)
 import           Language.TLAPlus.Syntax
 import           Text.ParserCombinators.Parsec     (ParseError, runParser)
 import           Text.ParserCombinators.Parsec.Pos as PPos
+
+import           Data.Map                          as Map hiding (map)
+import           Data.Set                          as Set hiding (map)
 
 instance Lift PPos.SourcePos where
   lift p = [| $(liftData p) |]
@@ -40,16 +45,41 @@ deriving instance Lift AS_UnitDef
 deriving instance Lift AS_OperatorHead
 deriving instance Lift AS_Expression
 
+instance Lift (Map VA_Value VA_Value) where
+  lift p = [| $(liftData p) |]
+instance Lift (Set VA_Value) where
+  lift p = [| $(liftData p) |]
+deriving instance Lift VA_Value
+
 tlaExpr :: String -> Q Exp
 tlaExpr str = do
   case parseTLAExpr str of
-    Left err  -> error (show err)
-    Right tag -> [| tag |]
+    Left err -> error (show err)
+    Right e  -> [| e |]
 
 tla :: QuasiQuoter
 tla = QuasiQuoter tlaExpr err err err
-  where err = error "Only defined for values 2"
+
+tla_e :: QuasiQuoter
+tla_e = tla
 
 parseTLAExpr :: String -> Either ParseError AS_Expression
 parseTLAExpr s =
   runParser expression mkState "" s
+
+
+tlaValue :: String -> Q Exp
+tlaValue str = do
+  case parseTLAExpr str of
+    Left err  -> error (show err)
+    Right expr ->
+      case evalE [] expr of
+        Left err ->
+          error (ppError err)
+        Right v ->
+          [| v |]
+
+tla_v :: QuasiQuoter
+tla_v = QuasiQuoter tlaValue err err err
+
+err = error "Only defined for values 2"
--- tlaplus-tests/src/Examples/SimpleExpr.hs
+++ tlaplus-tests/src/Examples/SimpleExpr.hs
@@ -7,31 +7,31 @@ import           Language.TLAPlus.Eval
 
 e0 :: (EnvExpr, AS_Expression)
 e0 =
-  let servers =     [tla|{"A","B"} |]
-      serverState = [tla|[A |-> "ready",
-                          B |-> "ready"] |]
-   in ([ ("Servers",     servers),
-         ("serverState", serverState)
+  let servers =     [tla_v|{"A","B"} |]
+      serverState = [tla_v|[A |-> "ready",
+                            B |-> "ready"] |]
+   in ([ (mkName "Servers",     servers),
+         (mkName "serverState", serverState)
        ],
-       [tla|\A server \in Servers:
-              serverState[server] \in {"ready"}
+       [tla_e|\A server \in Servers:
+                serverState[server] \in {"ready"}
        |])
 -- TRUE
 
 
 e1 :: (EnvExpr, AS_Expression)
 e1 = ([],
-      [tla|LET x == {x \in 1..3: TRUE} IN x |])
+      [tla_e|LET x == {x \in 1..3: TRUE} IN x |])
 -- {1,2,3}
 
 
 e2 :: (EnvExpr, AS_Expression)
 e2 =
   ([],
-   [tla|LET a == {2*x : x \in 1 .. 3}
-            b == 1
-         IN /\ a \subseteq 2..6
-            /\ \E z \in 1..3: z = b
+   [tla_e|LET a == {2*x : x \in 1 .. 3}
+              b == 1
+           IN /\ a \subseteq 2..6
+              /\ \E z \in 1..3: z = b
    |])
 -- TRUE
 
@@ -39,34 +39,26 @@ e2 =
 e3 :: (EnvExpr, AS_Expression)
 e3 =
   ([],
-   [tla|LET Nat == 0..100
-            factorial[n \in Nat] ==
-              IF n = 0
-              THEN 1
-              ELSE n * factorial[n-1]
-         IN factorial[4]
+   [tla_e|LET Nat == 0..100
+              factorial[n \in Nat] ==
+                IF n = 0
+                  THEN 1
+                  ELSE n * factorial[n-1]
+           IN factorial[4]
    |])
 -- 24
 
 
 main = do
-  let (benv, expr) = e0
-  case mkEnv benv of
+  let (env, expr) = e0
+  putStrLn $ ppEnv env
+  case evalE env expr of
     Left err ->
-      putStrLn $ "Failed to evaluate env values: " ++ show err
-    Right env -> do
-      putStrLn $ ppEnv env
-      case evalE env expr of
-        Left err ->
-          putStrLn $ ppError err
-        Right v ->
-          putStrLn $ prettyPrintVA v
+      putStrLn $ ppError err
+    Right v ->
+      putStrLn $ prettyPrintVA v
 
 
 -- helpers
-type EnvExpr = [(String, AS_Expression)]
-
-mkEnv :: [(String, AS_Expression)] -> Either EvalError Env
-mkEnv = mapM (\(n, e) -> do
-                 v <- evalE [] e
-                 return (([],n), v))
+type EnvExpr = [( ([String],String), VA_Value)]
+mkName n = ([], n)
